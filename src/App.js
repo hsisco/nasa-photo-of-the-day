1df4469c893a444344b08119a0ee3bb6c618caa2
hsisco <hysensisco@gmail.com>
2019-10-10 13:22:29 -0700

    App axios and useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  const [card, setCard] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=r5sUbJ04T0QQB9ePgcmclsEuaScYcf7Ovny30X0n')
+    .then(response => {
+      console.log('Huzzah! card data returned', response.data);
+      setCard(response.data);
+    })
+    .catch(error => {
+      console.log('NASA data not returned', error)
+    })
+  },[]);
+
 }
 
 export default App;
